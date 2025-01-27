ea03d20186952ca443144fe700cd64d2b4c26bea
jaltrock <altrockweb@gmail.com>
2025-01-27 18:51:16 -0500

    Added State for updating Tasklist

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
 
-export const AddTask = () => {
+export const AddTask = ({ tasklist, setTasklist }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const date = new Date();
+    const newTask = {
+      id: date.getTime(),
+      name: e.target.task.value,
+      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+    };
+    setTasklist([...tasklist, newTask]);
+    e.target.task.value="";
+  };
+
   return (
-    <section className='addTAsk'>
-        <form>
-            <input type = "text" name='task' autoComplete='off' placeholder='add task' maxLength="25"/>
-            <button type='submit'>Add</button>
-        </form>
+    <section className="addTask">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          autoComplete="off"
+          placeholder="add task"
+          maxLength="25"
+        />
+        <button type="submit">Add</button>
+      </form>
     </section>
-  )
-}
-
-
+  );
+};
